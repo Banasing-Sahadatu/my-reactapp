723985ebf16b8f0071136f276b07324251b82152
Banasing-Sahadatu <110116966+Banasing-Sahadatu@users.noreply.github.com>
2022-08-11 17:20:44 +0000

    commit 1
    react is a framework

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from ".component/NavBar";
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <button></button>
+    </div>
+  );
+}
+
+export default App;
